805c1bde33edb2a8a5eca33c125740cb37c2487e
Julian <mail@yaman.pro>
2022-05-29 23:47:35 +0200

    fix(api): :bug: fix server domain

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://www.tooldb.dev';
+export const server = dev ? 'http://localhost:3000' : 'https://tooldb.dev';
